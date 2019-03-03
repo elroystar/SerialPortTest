d12d41e5c55142a159da23c53a6e3c466461152a
ziyichen <ziyichen@huashenghaoche.com>
2019-03-03 11:31:18 +0800

    更新串口命令

--- app/src/main/java/com/lotus/serialporttest/CmdConstance.java
+++ app/src/main/java/com/lotus/serialporttest/CmdConstance.java
@@ -5,25 +5,46 @@ package com.lotus.serialporttest;
  */
 public class CmdConstance {
     // 开始收币
-    public final static String START_COIN = "0x1A";
+    public final static String START_COIN = "0X1A";
     // 继续收币
-    public final static String CONTINUE_COIN = "0x1B";
+    public final static String CONTINUE_COIN = "0X1B";
     // 停止收币
-    public final static String STOP_COIN = "0x1C";
+    public final static String STOP_COIN = "0X1C";
     // 5泰铢
-    public final static String FIVE_THAI_BAHT = "0x01";
+    public final static String FIVE_THAI_BAHT = "0X01";
     // 10泰铢
-    public final static String TEN_THAI_BAHT = "0x02";
+    public final static String TEN_THAI_BAHT = "0X02";
     // 1欧元
-    public final static String ONE_EURO = "0x03";
+    public final static String ONE_EURO = "0X03";
     // 1美元
-    public final static String ONE_DOLLAR = "0x04";
-    // 洗衣机回复指令
-    public final static String MACHINE_1 = "0x01";
-    public final static String MACHINE_2 = "0x02";
-    public final static String MACHINE_3 = "0x03";
-    public final static String MACHINE_4 = "0x04";
-    public final static String MACHINE_5 = "0x05";
-    public final static String MACHINE_6 = "0x06";
+    public final static String ONE_DOLLAR = "0X04";
+    // 注册询问指令
+    public final static String REGISTER_ASK = "0X51";
+    // 设置已注册状态指令
+    public final static String REGISTERED = "0X01";
+    // 消毒水泵开
+    public final static String TEST_DISINFECTION_OPEN = "0X71";
+    // 消毒水泵关
+    public final static String TEST_DISINFECTION_CLOSE = "0X72";
+    // 洗衣液泵开
+    public final static String TEST_WASHING_LIQUID_OPEN = "0X73";
+    // 洗衣液泵关
+    public final static String TEST_WASHING_LIQUID_CLOSE = "0X74";
+    // 柔顺剂泵开
+    public final static String TEST_SOFTENING_OPEN = "0X75";
+    // 柔顺剂泵关
+    public final static String TEST_SOFTENING_CLOSE = "0X76";
+    // 进水阀开
+    public final static String TEST_WATER_IN_OPEN = "0X77";
+    // 进水阀关
+    public final static String TEST_WATER_IN_CLOSE = "0X78";
+    // 排水阀开
+    public final static String TEST_WATER_OUT_OPEN = "0X85";
+    // 排水阀关
+    public final static String TEST_WATER_OUT_CLOSE = "0X86";
+    // 短程序开
+    public final static String TEST_SHORT_PROGRAM_OPEN = "0X7D";
+    // 短程序关
+    public final static String TEST_SHORT_PROGRAM_CLOSE = "0X8F";
 
 }
