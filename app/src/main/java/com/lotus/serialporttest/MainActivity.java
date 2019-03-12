d6af3a1892793be7129c8a64390e63151d36021f
ziyichen <ziyichen@huashenghaoche.com>
2019-03-12 18:53:42 +0800

    模拟洗衣机apk

--- app/src/main/java/com/lotus/serialporttest/MainActivity.java
+++ app/src/main/java/com/lotus/serialporttest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lotus.serialporttest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,7 +23,22 @@ public class MainActivity extends AppCompatActivity {
         //注册EventBus
         EventBus.getDefault().register(this);
 
-        findViewById(R.id.open).setOnClickListener(new View.OnClickListener() {
+        serialPortUtil = new SerialPortUtil();
+        serialPortUtil.openSerialPort();
+
+        // 返回安卓
+        findViewById(R.id.bt_android).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 将应用退到桌面上，保留自身
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                startActivity(intent);
+            }
+        });
+
+        /*findViewById(R.id.open).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 serialPortUtil = new SerialPortUtil();
@@ -35,9 +51,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 serialPortUtil.closeSerialPort();
             }
-        });
+        });*/
 
-        findViewById(R.id.fiveThai).setOnClickListener(new View.OnClickListener() {
+        /*findViewById(R.id.fiveThai).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 serialPortUtil.sendSerialPort(CmdConstance.FIVE_THAI_BAHT);
@@ -101,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 serialPortUtil.sendSerialPort("06060606");
             }
-        });
+        });*/
     }
 
     /**
@@ -112,13 +128,22 @@ public class MainActivity extends AppCompatActivity {
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEventMainThread(String string) {
         Log.d("C09Activity", "获取到了从传感器发送到Android主板的串口数据");
-        if (string.contains(CmdConstance.REGISTERED)) {
+        /*if (string.contains(CmdConstance.REGISTERED)) {
             String id = string.substring(0, string.length() - 2);
             serialPortUtil.sendSerialPort(id);
         }
         if (string.contains(CmdConstance.RESET)) {
             String id = string.substring(0, string.length() - 2);
             serialPortUtil.sendSerialPort(id);
+        }*/
+        if (string.contains("01010101")) {
+            serialPortUtil.sendSerialPort("01010101");
+        }
+        if (string.contains(CmdConstance.REGISTER_ASK)) {
+            serialPortUtil.sendSerialPort("01010101");
+        }
+        if (string.contains(CmdConstance.RESET)) {
+            serialPortUtil.sendSerialPort("01010101");
         }
         Toast.makeText(MainActivity.this, "接收到串口指令：" + string, Toast.LENGTH_SHORT).show();
     }
